f1ecac87d955b44003fb58e9f331e0350d56f7d7
illoquehambre <illoquehambre@gmail.com>
2022-05-15 19:29:26 +0200

    error conversiond e tipos en el DDl

--- CafeteriaLaSenioraPop/src/main/resources/import.sql
+++ CafeteriaLaSenioraPop/src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into categoria (id, nombre) values (NEXTVAL('hibernate_sequence'),'Ensaladas');
-insert into categoria (id, nombre) values (NEXTVAL('hibernate_sequence'),'Carnes');
-insert into categoria (id, nombre) values (NEXTVAL('hibernate_sequence'),'Bebidas');
-insert into producto (id, nombre, precio, cantidad, url) values (NEXTVAL('hibernate_sequence'),'Ensalada César', 51495.5, 54, 'srvrsvrs');
-insert into producto (id, nombre, precio, cantidad, url) values (NEXTVAL('hibernate_sequence'),'Entrecot', 51495.5, 54, 'scvesvse');
-insert into producto (id, nombre, precio, cantidad, url) values (NEXTVAL('hibernate_sequence'),'Solomillo', 51495.5, 54, 'vwbvw');
+insert into categoria (id, nombre) values (('hibernate_sequence'),'Ensaladas');
+insert into categoria (id, nombre) values (('hibernate_sequence'),'Carnes');
+insert into categoria (id, nombre) values (('hibernate_sequence'),'Bebidas');
+insert into producto (id, nombre, precio, cantidad, url, categoria_id) values (('hibernate_sequence'),'Ensalada César', 51495.5, 54, 'srvrsvrs', 1);
+insert into producto (id, nombre, precio, cantidad, url, categoria_id) values (('hibernate_sequence'),'Entrecot', 51495.5, 54, 'scvesvse', 2);
+insert into producto (id, nombre, precio, cantidad, url, categoria_id) values (('hibernate_sequence'),'Solomillo', 51495.5, 54, 'vwbvw', 2);
