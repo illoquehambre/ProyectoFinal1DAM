2b30989ee9aa29c3452c3d0922d86a6688c2ed8d
Ignacio Moreno <Ignacio.moreno.gomez2003@gmail.com>
2022-05-17 09:13:07 +0200

    ticket no operativo

--- CafeteriaLaSenioraPop/src/main/java/com/salesianostriana/dam/clasesproyecto/controller/TicketController.java
+++ CafeteriaLaSenioraPop/src/main/java/com/salesianostriana/dam/clasesproyecto/controller/TicketController.java
@@ -1,8 +1,85 @@
 package com.salesianostriana.dam.clasesproyecto.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.salesianostriana.dam.clasesproyecto.model.Categoria;
+import com.salesianostriana.dam.clasesproyecto.model.LineaDeVenta;
+import com.salesianostriana.dam.clasesproyecto.model.Producto;
+import com.salesianostriana.dam.clasesproyecto.servicios.LineaDeVentaServicio;
+import com.salesianostriana.dam.clasesproyecto.servicios.ProductoServicio;
+import com.salesianostriana.dam.clasesproyecto.servicios.TicketServicio;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+
+
+@RequiredArgsConstructor
+
+@Controller
 public class TicketController {
 
+	@Autowired
+	private TicketServicio ticketServicio;
+
+	@Autowired
+	private ProductoServicio productoServicio;
+	
+	@Autowired
+	private LineaDeVentaServicio lineaDeVentaServicio;
 
 	
-	
+
+	@GetMapping("/carrito")
+	public String showCarrito(Model model) {
+		
+		List <LineaDeVenta> lineaDeVenta = new ArrayList<LineaDeVenta>();
+		for (LineaDeVenta ticket : lineaDeVenta) {
+			lineaDeVenta.add(ticket);
+		}
+		model.addAttribute("lineaDeVenta", lineaDeVenta);
+		
+		return "ticket";
+	}
+
+	@GetMapping("/productoACarrito/{id}")
+	public String productoACarrito(@PathVariable("id") Long id, Model model) {
+
+		ticketServicio.addLineaDeVenta(lineaDeVentaServicio.findById(id));
+
+		return "redirect:/carrito";
+	}
+
+	@GetMapping("/borrarProducto/{id}")
+	public String removeProductFromCart(@PathVariable("id") Long id) {
+
+		TicketServicio.removeProducto(productoServicio.findById(id));
+		return "redirect:/carrito";
+	}
+
+	@ModelAttribute("total_carrito")
+	public Double totalCarrito() {
+		double total = 0.0;
+		List <LineaDeVenta> carrito = TicketServicio.getProductosCarrito();
+		
+		if (carrito != null) {
+			for (Producto p : carrito.keySet()) {
+				total += p.getPrecio() * carrito.get(p);
+			}
+			return total;
+		}
+
+		return 0.0;
+	}
+
 }
--- CafeteriaLaSenioraPop/src/main/java/com/salesianostriana/dam/clasesproyecto/servicios/TicketServicio.java
+++ CafeteriaLaSenioraPop/src/main/java/com/salesianostriana/dam/clasesproyecto/servicios/TicketServicio.java
@@ -1,16 +1,44 @@
 package com.salesianostriana.dam.clasesproyecto.servicios;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 
+import com.salesianostriana.dam.clasesproyecto.model.LineaDeVenta;
 import com.salesianostriana.dam.clasesproyecto.model.Producto;
 import com.salesianostriana.dam.clasesproyecto.model.Ticket;
 import com.salesianostriana.dam.clasesproyecto.repositories.TicketRepository;
 import com.salesianostriana.dam.clasesproyecto.servicios.base.ServicioBaseImpl;
 
 @Service
+@Scope (value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class TicketServicio extends ServicioBaseImpl<Ticket, Long, TicketRepository>{
+	
+	
+	private  List<LineaDeVenta> ticket= new ArrayList <LineaDeVenta> ();
+	
+	public  List<LineaDeVenta> getProductosCarrito() {
+		// TODO Auto-generated method stub
+		return Collections.unmodifiableList(ticket);
+	}
+
+	public void addLineaDeVenta (Optional<LineaDeVenta> optional) {
+		
+		ticket.add(optional.get());
+	}
+
+	public void removeProducto(Optional<Producto> p) {
+		ticket.remove(p);
+		
+	}
 
 	
 }
